2f4801ca2900d972b85f680c502473fedb3a285b
malirezaie <m.alirezaie@gmail.com>
2017-12-04 17:03:15 -0500

    remove repl tests
    remove repl tests

--- UITests/Tests/ReplTests.cs
+++ UITests/Tests/ReplTests.cs
@@ -6,17 +6,17 @@ using Xamarin.UITest;
 
 namespace FaceOff.UITests
 {
-	public class ReplTests : TestSetUp
-	{
-		public ReplTests(Platform platform):base(platform)
-		{
-		}
+	//public class ReplTests : TestSetUp
+	//{
+	//	public ReplTests(Platform platform):base(platform)
+	//	{
+	//	}
 
-		[Test]
-		public void Repl()
-		{
-			app.Repl();
-		}
-	}
+	//	[Test]
+	//	public void Repl()
+	//	{
+	//		app.Repl();
+	//	}
+	//}
 }
 
