9f68ae6cd5517722b509a6e4ba55907e20c03467
malirezaie <m.alirezaie@gmail.com>
2018-11-08 02:15:15 -0500

    update build script and info plist
    update build script and info plist

--- iOS/appcenter-pre-build.sh
+++ iOS/appcenter-pre-build.sh
@@ -2,3 +2,4 @@
 
 find .. -name Keys.cs -exec sed -i -e 's/emotionkey/'"$EMOTION_KEY"'/g' {} \;
 find .. -name Keys.cs -exec sed -i -e 's/ioskey/'"$IOS_APPCENTER_KEY"'/g' {} \;
+find .. -name Info.plist -exec sed -i -e 's/APPSECRET/'"$IOS_APPCENTER_KEY"'/g' {} \;
